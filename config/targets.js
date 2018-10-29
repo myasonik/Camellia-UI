ee77f15dd010a6190f2522096ae0f03c4cc34fdb
Michail Yasonik <michail.yasonik@crowdstrike.com>
2018-10-28 23:36:06 -0700

    project setup

--- config/targets.js
+++ config/targets.js
@@ -1,9 +1,12 @@
-/* eslint-env node */
+'use strict';
+
+const browsers = [
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions',
+  'last 1 Edge versions'
+];
+
 module.exports = {
-  browsers: [
-    'ie 9',
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions'
-  ]
+  browsers
 };
